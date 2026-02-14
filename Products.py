7d4050db36986344366f0dd4f652a427907025ae
UadeIriajen <151917712+UadeIriajen@users.noreply.github.com>
2026-02-14 23:14:53 +0100

    Implement CRUD operations for products
    Added CRUD operations for products including retrieval, addition, updating, and deletion.

--- Products.py
+++ Products.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from model import Product
-
+from database import session 
 app = FastAPI()
 
 @app.get("/")
@@ -37,3 +37,41 @@ products = [
         quantity=20
         ),
 ]
+
+
+@app.get("/products")
+def get_all_products():
+    db = session()
+    db.query(Product).all()
+    return products
+
+
+@app.get("/products/{product_id}")
+def get_product_by_id(product_id: int):
+    for product in products:
+        if product.id == product_id:
+            return product
+    return {"error": "Product not found"}
+
+@app.post("/product")
+def add_product(product: Product):
+    products.append(product)
+    return {"message": "Product added successfully", "product": product}
+
+@app.put("/products/{product_id}")
+def update_products(product_id : int, updated_product: Product):
+    for i in range(len(products)):
+        if products[i].id == product_id:
+            products[i] = updated_product
+            return {"message": "Product updated successfully", "product": updated_product}
+    
+    return {"error": "Product not found"}
+
+@app.delete("/products/{product_id}")
+def delete_product(product_id: int):
+    for i in range(len(products)):
+        if products[i].id == product_id:
+            del products[i]
+            return "Product deleted" 
+    
+    return "Product not found"
