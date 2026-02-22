85617c1945d552c43a1b8210ceb8f8df7c7badbc
UadeIriajen <151917712+UadeIriajen@users.noreply.github.com>
2026-02-22 23:57:41 +0100

    Add files via upload
    from database import Base
    from sqlalchemy import Column, Integer, String, Float
    
    class Product(Base):
        
        __tablename__ = "products"
    
        id = Column(Integer, primary_key=True, index=True)
        name = Column(String, index=True)
        description = Column(String)
        price = Column(Float)
        quantity = Column(Integer)

---
+++ database_models.py
@@ -0,0 +1,14 @@
+from database import Base
+from sqlalchemy import Column, Integer, String, Float
+
+class Product(Base):
+    
+    __tablename__ = "products"
+
+    id = Column(Integer, primary_key=True, index=True)
+    name = Column(String, index=True)
+    description = Column(String)
+    price = Column(Float)
+    quantity = Column(Integer)
+   
+   
